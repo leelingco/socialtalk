b8911086d4bc9f5e51207babb4136a4b7adce907
weiyan <weiyan42@gmail.com>
2013-02-13 23:10:08 -0800

    change span to button to make it more obvious

--- web/index.php
+++ web/index.php
@@ -76,7 +76,7 @@
 					<ul class='steps'>
 						<li><img src="images/circle-1.png" alt="1" class='step-image'><a></a><button onclick='createSession()' id='create-chartroom'>create a chatroom</button></button></li>
 						<li id='twitter-section'><img src="images/circle-2.png" alt="2" class='step-image'><img src='images/sign-in-with-twitter-gray.png' id='twitter-button' style='width:158px;height:28px' onclick='twitterConnect()'></li>
-						<li><img src="images/circle-3.png" alt="3" class='step-image'><a>send the invite</a><span id='twitter-invite-to'></span><br/><textarea id='share-url'></textarea><span class='button' id='send-invite' onclick='sendTwitterInvite()'>Send</span></li>
+						<li><img src="images/circle-3.png" alt="3" class='step-image'><a>send the invite</a><span id='twitter-invite-to'></span><br/><textarea id='share-url'></textarea><button class='button' id='send-invite' onclick='sendTwitterInvite()'>Send</button></li>
 				</div>
 				<div class='right'><div style='width:600px;height:400px;float:left'><img id='video-window' src="http://placehold.it/600x450&text=Video"></div>				
 			</div>
